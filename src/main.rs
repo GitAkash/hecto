33720cf5df41c2d1f58ad77fae957ac165d9b3fe
Akash <akashmondalnl@gmail.com>
2025-10-28 20:27:59 +0100

    Press q to quit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::io::{self, Read};
 
-fn main(){
-    for b in io::stdin().bytes(){
+fn main() {
+    for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
-        println!("{}",c);
+        if c == 'q' {
+            break;
+        }
+        println!("{}", c);
     }
 }
